2e0fc3843d90de16de2f913e4355f2c82ecf332f
wangxiaoshuo <wangxiaoshuo@cvte.com>
2025-05-09 17:58:55 +0800

    feat: 浏览器环境模拟微队列

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,19 @@ export function flat(arr: any[]) {
     return prev;
   }, []);
 }
+
+export function runMicrotask(fn) {
+  if (typeof globalThis.MutationObserver !== 'undefined') {
+    const observer = new MutationObserver(fn as any);
+    const textNode = document.createTextNode('1');
+    observer.observe(textNode, {
+      characterData: true,
+    });
+    textNode.data = '2';
+    return;
+  } else {
+    setTimeout(() => {
+      fn();
+    }, 0);
+  }
+}
